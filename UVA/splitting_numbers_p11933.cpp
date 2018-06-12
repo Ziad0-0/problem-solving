5802b2f8ec7f8102ee590c9a0e16f7f61459d325
Ziad Khaled <ziadkhaled1419@gmail.com>
2018-06-12 18:10:43 +0200

    some uva problems that  I have solved

--- UVA/splitting_numbers_p11933.cpp
+++ UVA/splitting_numbers_p11933.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-void printSet(int vS) {                         // in binary representation
-  printf("S = %2d = ", vS);
-  stack<int> st;
-  while (vS)
-    st.push(vS % 2), vS /= 2;
-  while (!st.empty())                         // to reverse the print order
-    printf("%d", st.top()), st.pop();
-  printf("\n");
-}
-
 int main()
 {
     unsigned int n;
